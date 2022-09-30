5bb5d5a63eab06d29eb5dcb7ad1bc1c6f9b827c0
alan vergara <alan_vergara_p@hotmail.com>
2022-09-30 14:42:10 -0500

    ask and aswer methods fully implemented, app works as xpected

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,17 @@
+class QuestionsController < ApplicationController
+  def ask
+    params[:answer]
+  end
+
+  def answer
+    @answer = ask
+    @response = ''
+    if @answer == 'I am going to work'
+      @response = 'Great'
+    elsif @answer.end_with?('?')
+      @response = 'Silly question, get dressed and go to work!.'
+    else
+      @response = 'I don\'t care, get dressed and go to work!'
+    end
+  end
+end
